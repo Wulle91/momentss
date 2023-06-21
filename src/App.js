3a5fe00c9316831ba6d3608f8c6f9420f603c1e7
Wulle91 <mvuckovi91@gmail.com>
2023-06-21 15:07:34 +0000

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import styles from './App.module.css';
 import Button from 'react-bootstrap/Button';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom' 
+
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route exact path="/signin/" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup/" render={() => <h1>Sign upsi</h1>} />
+          <Route render={() => <h1>Page not found.</h1>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
