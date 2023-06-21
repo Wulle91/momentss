3e2f8b7f74a7fc113de170e451de82cd6cae5232
Wulle91 <mvuckovi91@gmail.com>
2023-06-21 19:56:47 +0000

    ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import styles from './App.module.css';
 import Button from 'react-bootstrap/Button';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
-import { Route, Switch } from 'react-router-dom' 
+import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults'
+import SignUpForm from './pages/auth/SignUpForm';
 
 
 function App() {
@@ -13,7 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home Page</h1>} />
           <Route exact path="/signin/" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup/" render={() => <h1>Sign upsi</h1>} />
+          <Route exact path="/signup/" render={() => <SignUpForm />} />
           <Route render={() => <h1>Page not found.</h1>}/>
         </Switch>
       </Container>
